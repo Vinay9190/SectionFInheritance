f20f4f7d0858788a2027792e83162f85c35cd2ae
vinay <vinay.yadav_cs19@gla.ac.in>
2020-08-25 11:42:10 +0530

    initial commit

---
+++ src/definitions/MobilePhone.java
@@ -0,0 +1,8 @@
+package definitions;
+
+public class MobilePhone extends Phone  {
+   // MobilePhone extends Phone
+    // MobilePhone IS a type of phone
+}
+
+// Two entities; every entity is a class,
--- src/inheritance/InheritanceDemo.java
+++ src/inheritance/InheritanceDemo.java
@@ -1,10 +1,11 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 25/08/20
- *  Time: 11:05 AM
+ *  Author: Vinay Yadav
  *  File Name : InheritanceDemo.java
  * */
 package inheritance;
 
 public class InheritanceDemo {
+
+
+
 }
