38833eb992969de66e63c6d21ac5b1eb39e105de
vinay <vinay.yadav_cs19@gla.ac.in>
2020-08-25 16:18:24 +0530

    write all heading to learn inheritance

--- src/inheritance/InheritanceDemo.java
+++ src/inheritance/InheritanceDemo.java
@@ -1,11 +1,21 @@
-/*  Created by IntelliJ IDEA.
- *  Author: Vinay Yadav
- *  File Name : InheritanceDemo.java
- * */
+
 package inheritance;
 
 public class InheritanceDemo {
 
+    // 1. What is Inheritance?
 
+    // Inheritance(noun) : something that is or may be inherited
+    // Inheritance(verb) : to derive something.
+
+    // Inheritance(verb) :
+
+    // 2.How to use Inheritance to re-use code/programs/software that is already develop
+    // 3.Observe the 'extends' keyword.
+    // 4.What is a 'super' class and what is 'sub' class .
+    // 5.How we can access the member of the super class from sub class.
+    // 6.Constructor Hierarchy
+    // 7.Method Overridding,Dynamic Method Dispatch, Abtract class & Interfaces.
+    // 8.How to model our programs by using all the concept of written above.
 
 }
