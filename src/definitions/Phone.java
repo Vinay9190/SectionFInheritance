bb0514d537ce13b9d3f763639ec1fc28694117af
vinay <vinay.yadav_cs19@gla.ac.in>
2020-08-25 16:45:17 +0530

    create a private field  of long datatype  and contactNumber and add getter and setter method for the private field

--- src/definitions/Phone.java
+++ src/definitions/Phone.java
@@ -1,4 +1,13 @@
 package definitions;
 
 public class Phone {
+    private long contactNumber; // contact number of a phone itself.
+
+    public long getContactNumber() {
+        return contactNumber;
+    }
+    public void setContactNumber() {
+        this.contactNumber = contactNumber;
+    }
+
 }
