bca446192b169f4cbc66597d6068def20f237812
vinay <vinay.yadav_cs19@gla.ac.in>
2020-08-25 16:39:16 +0530

    We have discuss about the What is Inheritance and How we Re-use the code.

--- src/inheritance/InheritanceDemo.java
+++ src/inheritance/InheritanceDemo.java
@@ -4,13 +4,24 @@ package inheritance;
 public class InheritanceDemo {
 
     // 1. What is Inheritance?
+    // Inheritance (Object-Oriented): Every entity is a class!
+    // Inheritance (noun) : something that is or may be inherited
+    // Inheritance (verb) : to derive something OR " is a mechanism by which a can acquire properties(fields/variables) and methods
+    //  of another class, related to it.
 
-    // Inheritance(noun) : something that is or may be inherited
-    // Inheritance(verb) : to derive something.
+    // 2.How to use Inheritance to re-use code/programs/software that is already developed and tested.
 
-    // Inheritance(verb) :
+    // Two types of such software that exists in the world.
+    // 1. Open Source Software:
+    // we can observe the source code and we might able to modify it .
+    // Ex : Linux kernel
+    // 2. Closed Source Software:
+    // we cannot observe the source code and we cannot modify it.
+    // ex- Google Chrome, Microsoft Windows etc
 
-    // 2.How to use Inheritance to re-use code/programs/software that is already develop
+    // Re-use of code/programs/software.
+    // To Modify the current version of the software, without starting from the beginning.
+
     // 3.Observe the 'extends' keyword.
     // 4.What is a 'super' class and what is 'sub' class .
     // 5.How we can access the member of the super class from sub class.
