62b628cfadc8b101fe5e242b6f7c910e28a5a6e9
vinay <vinay.yadav_cs19@gla.ac.in>
2020-08-27 16:07:17 +0530

    add explanation for public class Truck

---
+++ src/definitions/transport/Truck.java
@@ -0,0 +1,11 @@
+package definitions.transport;
+
+// If we create a public class in Java,
+// the name of the class (Truck)
+// the name of the file (Truck.java)
+// should be  exactly the same!
+
+public class Truck extends Vehicle {
+
+}
+}
