4cabf2e0f92772cc897d5f8593faaa0f19925f45
vinay <vinay.yadav_cs19@gla.ac.in>
2020-09-04 00:22:11 +0530

    call the object method of class displayA() method

--- src/definitions/examples/Example5.java
+++ src/definitions/examples/Example5.java
@@ -2,7 +2,11 @@ package definitions.examples;
 
 public class Example5 {
     public static void main(String[] args) {
-
+    A2 objectA2 = new A2();
+    B2 objectB2 = new B2();
+    objectA2.setValues(5,6);
+    objectA2.m = 10;
+    objectA2.displayA();
     }
 }
 
@@ -18,7 +22,7 @@ class A2 {
     public int m;
 
     void displayA() {
-        System.out.println("j = " + j + ", k = " + k);
+        System.out.println("j = " + j + ", k = " + k + " m = " + m);
     }
 
     public int getJ() {
@@ -32,4 +36,9 @@ class A2 {
 
 class B2 extends A2 {
     int p ;
+
+    void displayB2() {
+        System.out.println("p = " + p);
+        System.out.println("m = " + m);
+    }
 }
