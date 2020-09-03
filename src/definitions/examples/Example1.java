23aced72bef36d0934e24752522a342631f1c890
vinay <vinay.yadav_cs19@gla.ac.in>
2020-09-03 22:09:47 +0530

    completer Example1

--- src/definitions/examples/Example1.java
+++ src/definitions/examples/Example1.java
@@ -2,12 +2,22 @@ package definitions.examples;
 
 public class Example1 {
     public static void main(String[] args) {
-
+        DemoA objectA = new DemoA();
+        objectA.displayA();
+        DemoB objectB = new DemoB();
+        objectB.displayA();
     }
 }
 
  class DemoA {
-    public void displayA() {
+    public void displayA() { // 1
         System.out.println("Parent Class Method.");
+    }
+}
+
+class DemoB extends DemoA {
+    @Override
+    public void displayA() { // 2 - Method Overriding
+        System.out.println("child Class Method");
     }
 }
