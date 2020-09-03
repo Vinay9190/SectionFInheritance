bfd0f275cb25d4403132f4bd0f49a86a4ff56572
vinay <vinay.yadav_cs19@gla.ac.in>
2020-09-03 23:41:09 +0530

    create an object of class B1, set the values and call the product

--- src/definitions/examples/Example3.java
+++ src/definitions/examples/Example3.java
@@ -2,7 +2,9 @@ package definitions.examples;
 
 public class Example3 {
     public static void main(String[] args) {
-
+    B1 objectB1 = new B1();
+    objectB1.setvalues(10,8);
+    System.out.println(objectB1.product(10,9));
     }
 }
 
