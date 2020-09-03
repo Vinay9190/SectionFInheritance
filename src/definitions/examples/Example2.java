d84afee921eb57f53fa75db969a827480fcbe2a7
vinay <vinay.yadav_cs19@gla.ac.in>
2020-09-03 22:42:22 +0530

    Complete Example2

--- src/definitions/examples/Example2.java
+++ src/definitions/examples/Example2.java
@@ -1,8 +1,41 @@
 package definitions.examples;
 
 public class Example2 {
-
     public static void main(String[] args) {
+    D objectD = new D();
+    objectD.methodD(5);
+    objectD.methodA(4);
+    }
+}
 
+class A {
+    int j = 0;
+    public void methodA(int k) {
+        j = k;
+        System.out.println("Class A : j = " + j);
+    }
+}
+ class B extends A {
+    @Override
+    public  void methodA(int x) { //method overridding
+        j = x * x;
+        System.out.println("Class B : j = " + 1);
+    }
+    public void methodB() {
+        System.out.println("Class B");
+    }
+ }
+class C extends  A {
+    public void methodA(int k1) { // method overridding
+        j = k1 * 5;
+    }
+    public void methodC() {
+        System.out.println("Class C");
+    }
+}
+class D extends B {
+    public void methodD(int k) {
+        j = k * k;
+        System.out.println("Class D");
     }
 }
