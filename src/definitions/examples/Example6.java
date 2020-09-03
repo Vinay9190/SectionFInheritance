a9752abc9ce92e8714a662878648a354bf53c751
vinay <vinay.yadav_cs19@gla.ac.in>
2020-09-04 00:45:20 +0530

    create class SuperBike which extends class Bike

--- src/definitions/examples/Example6.java
+++ src/definitions/examples/Example6.java
@@ -5,15 +5,29 @@ public class Example6 {
 
     }
 }
+
 class Bike {
-    private int idNumber;
     public String modelName;
+    private final int idNumber;
+
+
+    public Bike(int idNumber, String modelName) {
+        this.idNumber = idNumber;
+        this.modelName = modelName;
+    }
+
+    public void displayBikeInformation() {
+        System.out.println("IdNumber = " + idNumber);
+        System.out.println("ModelName = " + modelName);
+    }
 }
-public Bike(int idNumber, String modelName) {
-    this.idNumber = idNumber;
-    this.modelName = modelName;
-}
-public void displayBikeInformation() {
-    System.out.println("IdNumber = " + idNumber);
-    System.out.println("ModelName = " + modelName);
+class SuperBike extends Bike {
+    private  double maxSpeed;
+
+    public SuperBike(int idNumber, String modelName, double maxSpeed) {
+        // the super keyword
+        super(idNumber,modelName);
+        this.maxSpeed = maxSpeed;
+
+    }
 }
