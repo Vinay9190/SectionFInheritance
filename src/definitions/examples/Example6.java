3ec9a4c8e71e02ac3567a14f93f1753e662f6b18
vinay <vinay.yadav_cs19@gla.ac.in>
2020-09-04 00:33:30 +0530

    add the field for Bike

--- src/definitions/examples/Example6.java
+++ src/definitions/examples/Example6.java
@@ -3,4 +3,9 @@ package definitions.examples;
 public class Example6 {
     public static void main(String[] args) {
 
-    }}
+    }
+}
+class Bike {
+    private int idNumber;
+    public String modelName;
+}
