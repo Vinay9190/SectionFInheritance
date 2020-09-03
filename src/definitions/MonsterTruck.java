db2afdba7c97681d35379876aa195dcc0889d3f7
vinay <vinay.yadav_cs19@gla.ac.in>
2020-09-03 21:14:04 +0530

    create MonsterTruck.java and access class Truck

---
+++ src/definitions/MonsterTruck.java
@@ -0,0 +1,11 @@
+package definitions;
+
+import  definitions.transport.Truck;
+// we can access the Truck Class
+// in a different class, even outside the package!
+
+
+public class MonsterTruck {
+
+    private Truck myMonsterTruck;
+}
