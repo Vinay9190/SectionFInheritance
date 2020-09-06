101d211e5bb94a47bbaa2b5f57ba5026461becbc
vinay <vinay.yadav_cs19@gla.ac.in>
2020-09-06 09:29:24 +0530

    create a getter method for class Airplane

--- src/definitions/examples/Example7.java
+++ src/definitions/examples/Example7.java
@@ -6,9 +6,13 @@ public class Example7 {
     }
 }
 class Airplane {
-    private  int airplanID;
+    private int airplanID;
 
     public Airplane(int airplaneID) {
         this.airplanID = airplaneID;
+    }
+
+    public int getAirplanID() {
+        return airplanID;
     }
 }
