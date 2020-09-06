9a901e243638ff65487a826a8819b34c898e02a6
vinay <vinay.yadav_cs19@gla.ac.in>
2020-09-06 11:45:01 +0530

    create Bank interface

--- src/interfaces/Example.java
+++ src/interfaces/Example.java
@@ -1,19 +1,17 @@
 package interfaces;
+// 1.Fields
+// i. all the fields declared inside an interface must be initialized.
+// ii. all the fields declared inside an interfaces are implicity declared as public.
+// iii. all the fields declared inside an interface are implicity declared as final i.e we
+// cannot change its value after is has initialized.
+// all the fields declared inside an interface are implicitly declared as static.
+// public static final <data Type> <variableName> = <initialValues>;
+// 2. Methods
+// i. all the methd declared inside a interface are implicitly declared as abstract.
+// ii. all the methods declared inside an interface are implicitly declared as public.
+// public abstract <dataType> <methodName> (<Parameters>)
 
 public interface Example {
 
-    // Fields
-    // i. all the fields declared inside an interface must be initialized.
-    // ii. all the fields declared inside an interfaces are implicity declared as public.
-    // iii. all the fields declared inside an interface are implicity declared as final i.e we
-    // cannot change its value after is has initialized.
-    // all the fields declared inside an interface are implicitly declared as static.
-    // public static final <data Type> <variableName> = <initialValues>;
-    public final int number = 4;
 
-    // 2. Methods
-    // i. all the methd declared inside a interface are implicitly declared as abstract.
-    // ii. all the methods declared inside an interface are implicitly declared as public.
-    // public abstract <dataType> <methodName> (<Parameters>)K
-    public abstract void method();
 }
