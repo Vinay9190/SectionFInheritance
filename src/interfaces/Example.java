383209e5b8a4ee71fb02c93fb215b4275226186f
vinay <vinay.yadav_cs19@gla.ac.in>
2020-09-07 23:17:00 +0530

    it is example to show interface

--- src/interfaces/Example.java
+++ src/interfaces/Example.java
@@ -1,17 +1,18 @@
 package interfaces;
-// 1.Fields
+                    // 1.Fields
 // i. all the fields declared inside an interface must be initialized.
 // ii. all the fields declared inside an interfaces are implicity declared as public.
 // iii. all the fields declared inside an interface are implicity declared as final i.e we
 // cannot change its value after is has initialized.
 // all the fields declared inside an interface are implicitly declared as static.
 // public static final <data Type> <variableName> = <initialValues>;
-// 2. Methods
+                  // 2. Methods
 // i. all the methd declared inside a interface are implicitly declared as abstract.
 // ii. all the methods declared inside an interface are implicitly declared as public.
 // public abstract <dataType> <methodName> (<Parameters>)
 
 public interface Example {
-
+ public final int number = 4;
+ public abstract void method();
 
 }
