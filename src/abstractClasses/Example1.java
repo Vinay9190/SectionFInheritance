d57b5e74650dc690e5e60e8829a07800ceb398a2
vinay <vinay.yadav_cs19@gla.ac.in>
2020-09-07 22:52:15 +0530

    create class Human

--- src/abstractClasses/Example1.java
+++ src/abstractClasses/Example1.java
@@ -1,6 +1,12 @@
 package abstractClasses;
 
 public class Example1 {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+
+    }
 
 }
+ class Human {
+
+ }
+
